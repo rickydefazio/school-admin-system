19838333084129cd73602dcbf98683375ce2e7ec
Ricky DeFazio <rickysdefazio@gmail.com>
2019-09-11 14:20:31 -0400

    Test API connection from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function App() {
+  // testing connection to api
+  fetch('http://localhost:5000/api/courses')
+    .then(res => res.json())
+    .then(data => console.log(data))
+
   return (
     <div className="App">
       
