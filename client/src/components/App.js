44bba3494206c3d76faf842e4a76960dcf39c966
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2019-09-11 21:54:32 -0400

    Move App.js to component folder

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Courses from './Courses';
+
+function App() {
+  return (
+    <div className="App">
+      <Courses />
+    </div>
+  );
+}
+
+export default App;
