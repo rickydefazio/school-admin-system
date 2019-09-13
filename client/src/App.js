4525c8cf6de925ba7fd7e9d6b2e0bf5d8d50a24c
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2019-09-13 13:31:07 -0400

    Add static courses page and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 import Courses from './components/Courses';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <hr />
       <Courses />
     </div>
   );
--- client/src/components/UserSignOut.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function UserSignOut() {
-  return (
-
-  )
-}
-
-export default UserSignOut;
