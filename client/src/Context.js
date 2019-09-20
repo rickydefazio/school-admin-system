94fb4f2a35e40bd370f744e3eab4ed8dbfb67015
Ricky DeFazio <rickysdefazio@gmail.com>
2019-09-20 15:33:50 -0400

    Persist authenticatedUser state

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Data from './Data';
+import Cookies from 'js-cookie';
 
 const Context = React.createContext(); 
 
 export class Provider extends Component {
   
   state = {
-    authenticatedUser: null
+    authenticatedUser: Cookies.getJSON('authenticatedUser') || null
   }
   
   constructor() {
@@ -44,12 +45,14 @@ export class Provider extends Component {
           authenticatedUser: user,
         };
       });
+      Cookies.set('authenticatedUser', JSON.stringify(user), { expires: 1 });
     }
     return user;
   }
 
   signOut = () => {
     this.setState({ authenticatedUser: null });
+    Cookies.remove('authenticatedUser');
   }
 }
 
